940af7c6ee7be65c2fe053457f8bc70b76402d70
Aman Mansuri <162823789+Mansuriamn@users.noreply.github.com>
2024-11-19 17:37:21 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -103,6 +103,6 @@ app.use((err, req, res, next) => {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port,'0.0.0.0', () => {
   console.log(`Server running at http://192.168.238.84:${port}`);
 });
